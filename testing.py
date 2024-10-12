a681d32506ea83b33726042f2f7e866386a4ae1e
Jinmyung Kevin Kim <kevkim0201@gmail.com>
2024-10-12 16:51:31 -0700

    Your commit message

---
+++ testing.py
@@ -0,0 +1,19 @@
+test_dataset[
+    {
+        'age' : 29  
+        'gender' : 'male'
+        'weight' : 85
+        'height' : 171
+        'disease' : 'none'
+        'fitness_goal' : 'lose 10 kg'
+    },
+    {
+        'age' : 16  
+        'gender' : 'female'
+        'weight' : 45
+        'height' : 159
+        'disease' : 'none'
+        'fitness_goal' : 'grow one cm taller'
+
+    }
+]
