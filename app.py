5902759ad43e09c2af5bd99b03eccf8a335efd29
Raiden Santos <raidensantos005@gmail.com>
2024-10-12 15:40:49 -0700

    test

--- app.py
+++ app.py
@@ -1,9 +1,28 @@
-from flask import Flask, render_template, request
 import os
+import re
+from flask import Flask, render_template, request
 from langchain.prompts import PromptTemplate
 from langchain.llms import OpenAI
 from langchain.chains import LLMChain
-import re
 
-import os
-import re
+os.environ['OPENAI_API_KEY'] = '[put ur key here]'
+
+app = Flask(__name__)
+
+llm_resto = OpenAI(temperature=0.6)
+#TODO: Put prompt template here 
+# prompt_template_resto = PromptTemplate()
+#testing
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# TODO: implement this method
+@app.route('/recommend', methods=['POST'])
+def recommend():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
